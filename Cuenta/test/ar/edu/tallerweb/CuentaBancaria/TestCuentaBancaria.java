87d51adfd08727aa4aa0e6ce1c4500c5c175be37
Matias Sanchez <matias.l.sanchez@gmail.com>
2015-04-20 09:58:54 -0300

    Se agregan casos de prueba
    se agregan casos para que de  error donde deberia dar

--- Cuenta/test/ar/edu/tallerweb/CuentaBancaria/TestCuentaBancaria.java
+++ Cuenta/test/ar/edu/tallerweb/CuentaBancaria/TestCuentaBancaria.java
@@ -6,17 +6,24 @@ import org.junit.Test;
 public class TestCuentaBancaria {
 	@Test
 	public void testDeposito(){
+		System.out.println("Test Deposito");
 		CuentaBancaria cuentaPrueba=new CuentaBancaria(0.0);
 		cuentaPrueba.deposito(1500.0);
 		Assert.assertTrue(1500.0 == cuentaPrueba.getSaldo());
+		cuentaPrueba.deposito(-1520);
+		Assert.assertTrue(-1520 != cuentaPrueba.getSaldo());
 	}
 	@Test
 	public void testExtraccion(){
+		System.out.println("Test Extraccion");
 		CuentaBancaria cuentaPrueba=new CuentaBancaria(1500.0);		
 		Assert.assertTrue(cuentaPrueba.extraer(80.0)==true);
+		Assert.assertTrue(cuentaPrueba.extraer(1420.0)==true);
+		Assert.assertTrue(cuentaPrueba.extraer(10)==true);
 	}
 	@Test
 	public void testGetSaldo(){
+		System.out.println("Test Consulta Saldo");
 		CuentaBancaria cuentaPrueba=new CuentaBancaria(0.0);		
 		Assert.assertTrue(cuentaPrueba.getSaldo()==0);
 	}
